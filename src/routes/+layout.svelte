340902ded42f17bdc14da7c7b4544c001c3b2400
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-11-21 10:12:21 +0530

    feat: create basic frontpage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<main class="min-h-screen flex flex-col items-center">
+	<slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
+	let showLabel = true;
+	let domains: string;
+
+	function changeLabelState() {
+		if (domains.length == 0) showLabel = true;
+		else if (domains.length > 0) showLabel = false;
+	}
+
+	function submit() {
+		domains = '';
+	}
+</script>
+
+<section class="flex flex-col pt-10 pb-5">
+	<h1 class="font-black text-center text-3xl">MX Lookup</h1>
+	<h2 class="text-center">Fetch mx records for any domain</h2>
+</section>
+
+<section class="w-2/3">
+	<form on:submit={submit} class="w-full flex flex-col relative">
+		{#if showLabel}
+			<label
+				in:fade={{ delay: 70, duration: 120, easing: quintOut }}
+				for="domains"
+				class="text-sm font-light p-2 flex flex-col md:flex-row"
+				><p>Enter domains seperated by commas.</p>
+				<p class="ml-0 md:ml-1 mt-1 md:mt-0">
+					<strong class="text-gray-400">Ex: </strong>
+					<span id="example">mail.google.com,mail.proton.me</span>
+				</p>
+			</label>
+		{/if}
+		<textarea bind:value={domains} on:input={changeLabelState} id="domains" />
+		<button type="submit" class="bg-green-900 rounded-md font-semibold py-1">Go</button>
+	</form>
+</section>
+
+<style lang="postcss">
+	label {
+		@apply absolute my-3 text-gray-500 cursor-text;
+	}
+	#example {
+		@apply bg-gray-900/50 rounded-md p-1 font-mono font-light;
+	}
+	textarea {
+		@apply h-24 resize-none mb-5;
+		@apply bg-gray-800 text-gray-300 rounded-md p-2 mt-3 text-sm overflow-auto;
+		@apply focus:outline-0 focus:ring-2 focus:ring-gray-600 transition-all;
+	}
+</style>
