0032ebaee9bf37ec92538db9ecdeaedb5a7d8d2c
Abhisheke Acharya <abh1sheke.acharya@gmail.com>
2023-11-23 19:09:40 +0530

    feat: add excel manip module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub mod excel;
 pub mod dns;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler!(dns::query_batcher))
+        .invoke_handler(tauri::generate_handler![excel::save_to, dns::query_batcher])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
